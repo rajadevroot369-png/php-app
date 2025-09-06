db9c1102eb72a5eec3c2d1ab25d12dcb7d06d77b
rajadevroot369-png <rajadevroot369@gmail.com>
2025-09-06 23:11:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,24 @@
 <?php
-$url = "https://www.tangrijewellers.in/web/commonRest/generateOTP.json";
-
-$payload = "mobileNo=8610131863&type=SIGNUP&emailId=";
-
-$headers = [
-    "Accept: application/json, text/javascript, */*; q=0.01",
-    "Accept-Language: en-US,en;q=0.9",
-    "Connection: keep-alive",
-    "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin: https://www.tangrijewellers.in",
-    "Referer: https://www.tangrijewellers.in/register",
-    "Sec-Fetch-Dest: empty",
-    "Sec-Fetch-Mode: cors",
-    "Sec-Fetch-Site: same-origin",
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0",
-    "X-Requested-With: XMLHttpRequest",
-    "sec-ch-ua: \"Chromium\";v=\"130\", \"Opera\";v=\"115\", \"Not?A_Brand\";v=\"99\"",
-    "sec-ch-ua-mobile: ?0",
-    "sec-ch-ua-platform: \"Windows\""
-];
-
 $ch = curl_init();
 
 curl_setopt_array($ch, [
-    CURLOPT_URL => $url,
+    CURLOPT_URL => "https://eraktkosh.mohfw.gov.in/BLDAHIMS/bloodbank/voluntryDonorsGroupRegistration.cnt?hmode=PRE_REGISTER_ADMIN_GET_OTP&mobile=8610131863",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => $payload,
-    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_HTTPHEADER => [
+        "Accept: */*",
+        "Referer: https://eraktkosh.mohfw.gov.in/BLDAHIMS/bloodbank/voluntryDonorsGroupRegistration.cnt",
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/130.0.0.0 Safari/537.36",
+        "X-Requested-With: XMLHttpRequest",
+    ],
 ]);
 
 $response = curl_exec($ch);
-
-if (curl_errno($ch)) {
-    echo "cURL Error: " . curl_error($ch);
-} else {
-    echo $response;
-}
-
+$error = curl_error($ch);
 curl_close($ch);
+
+if ($error) {
+    echo "cURL Error: " . $error;
+} else {
+    echo $response; // raw response from server
+}
 ?>
